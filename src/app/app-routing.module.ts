e00077fc7458b49107193304acaf88e8d812a9bb
kpushpakumara89 <kpushpakumarajd@gmail.com>
2021-11-06 11:13:32 +0530

    date configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/guest_panel',pathMatch:'full'},
+  { path: 'adminpanel', loadChildren: () => import('./module/admin-panel/admin-panel.module').then(m => m.AdminPanelModule) },
+  { path: 'guest_panel', loadChildren: () => import('./module/guest-panel/guest-panel.module').then(m => m.GuestPanelModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FileUploadModule} from 'ng2-file-upload';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatNativeDateModule} from "@angular/material/core";
+import {LocalStorageModule} from "angular-2-local-storage";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    FileUploadModule,
+    LocalStorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
